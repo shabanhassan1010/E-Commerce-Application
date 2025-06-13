433ae62d57ff85e24702a16c28dfdff320939668
Shaban Hassan <shabanhassan1010@gmail.com>
2025-06-13 05:34:53 +0300

    Add Custome ActionFilterAttribute

---
+++ E-Commerce.ApplicationLayer/ActionFilters/LogSensitiveActionAttribute.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace E_Commerce.ApplicationLayer.ActionFilters
+{
+    public class LogSensitiveActionAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            Debug.WriteLine("Sensitive Data");
+        }
+    }
+}
